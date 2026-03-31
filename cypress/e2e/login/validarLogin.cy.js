eeebe295cb6027276d178d387bfa07fea1925e04
matheusnogueiraQA <matheussantiago1@hotmail.com>
2026-03-31 07:38:42 -0300

    adicionando fixture e commands

--- cypress/e2e/login/validarLogin.cy.js
+++ cypress/e2e/login/validarLogin.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+import './commands'
+
+describe('Login', () => {
+
+  //  it('Crie um novo usuário com sucesso', () => {     })
+
+    it('Tente realizar login com usuário sem cadastro', () => {
+        cy.login(faker.internet.email(), faker.internet.password(10))
+
+        cy.contains('Your email or password is incorrect!')
+  .should('be.visible')
+    })
+
+   // it('Realize login com sucesso', () => { })
 })
